ead4b77e1d0b68d948de19982c551f7ad9f0fdbb
dominiquem <dominique.meaux@gmail.com>
2018-06-24 19:43:10 +0200

    onmouseover et onmouseout

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//changement d image au survol
+function Img1Change(img){
+  document.getElementById('img1').src = img;
+}
+function Img2Change(img){
+  document.getElementById('img2').src = img;
+}
+function Img3Change(img){
+  document.getElementById('img3').src = img;
+}
+function Img4Change(img){
+  document.getElementById('img4').src = img;
+}
+function Img5Change(img){
+  document.getElementById('img5').src = img;
+}
