d5974fc19a2fb2cc863dd1e98953fd903a8e5a55
dominiquem <dominique.meaux@gmail.com>
2018-06-25 09:28:30 +0200

    bonus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
 //changement d image au survol
-function Img1Change(img){
-  document.getElementById('img1').src = img;
-}
-function Img2Change(img){
-  document.getElementById('img2').src = img;
-}
-function Img3Change(img){
-  document.getElementById('img3').src = img;
-}
-function Img4Change(img){
-  document.getElementById('img4').src = img;
-}
-function Img5Change(img){
-  document.getElementById('img5').src = img;
+function ImgChange(id, img){
+  document.getElementById(id).src = img;
 }
